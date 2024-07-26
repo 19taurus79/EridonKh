57dd3cbf3b67b53315c8aac7d7045834e69f3bca
19taurus79 <19taurus79@gmail.com>
2024-07-26 15:14:07 +0300

    Переписал представление через класс

--- djangoProjectKh/urls.py
+++ djangoProjectKh/urls.py
@@ -19,10 +19,10 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path
 
-from EridonKh.views import index_view, remains_view
+from EridonKh.views import IndexView, remains_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("home/", index_view, name="home_page"),
+    path("home/", IndexView.as_view(), name="home_page"),
     path("remains/", remains_view, name="remains_page"),
 ] + debug_toolbar_urls()
